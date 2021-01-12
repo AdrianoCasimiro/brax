bf309df5906f5beaeb92ede67c2af531ff6fe4f5
adrianocasimiro <adrianocasi@gmail.com>
2021-01-12 15:29:33 -0300

    adicionado paginas de erro e login

--- braxcloud/settings.py
+++ braxcloud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +26,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'braxcloud.accounts',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -132,3 +133,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
